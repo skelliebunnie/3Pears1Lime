25f30e1b6f272a1b831c8c10f5ce36aa840ebff2
Margaret Ralston <pandahart@gmail.com>
2019-12-16 21:06:28 -0800

    first commit.

---
+++ public/index.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>3 Pears, 1 Lime</title>
+	<link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+	<div id="page-container" class="sidebar-none site-nav-none">
+		<header class="column">
+			<section class="col">
+				<h1 class="site-title text-center">3 Pears, 1 Lime</h1>
+			</section>
+			<section id="pears-lime" class="col no-pad no-margin">
+				<img src="../images/3 Pears 1 Lime - Header Fruit.png" alt="">
+			</section>
+		</header>
+		<main class="column">
+			<article id="find" class="col">
+				<section class="container">
+					<h2>Find a <span>Recipe</span></h2>
+					<img src="../images/FancySalad.png" alt="">
+				</section>
+			</article>
+			<article id="explore" class="col">
+				<section class="container">
+					<h2>Explore Cooking</h2>
+					<img src="../images/pancakes.png" alt="">
+				</section>
+			</article>
+			<article id="submit" class="col">
+				<section class="container">
+					<h2>Submit a Recipe</h2>
+					<img src="../images/Recipe Cards.png" alt="">
+				</section>
+			</article>
+		</main>
+		<aside class="column">
+			<article id="cocktails" class="col">
+				<section class="container">
+					<img src="../images/Lime - cocktails.png" alt="">
+					<h3>Cocktails</h3>
+				</section>
+			</article>
+			<article id="contact" class="col">
+				<h3>Contact</h3>
+			</article>
+		</aside>
+		<footer class="row copyright">
+			<section class="col col-12">
+				<p>Copyright &copy; 2019 <strong>3 Pears & A Lime</strong></p>
+			</section>
+		</footer>
+	</div>
+</body>
+</html>
