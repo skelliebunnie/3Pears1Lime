a04dc4a9829ba708eb17134fe60c1c2d5014b256
Skelliebunnie <pandahart@gmail.com>
2019-12-17 14:27:17 -0800

    looks good! contact form doesn't work & I want to tweak the input & label appearance, but otherwise the index is done!

--- public/index.php
+++ public/index.php
@@ -1,48 +1,70 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>3 Pears, 1 Lime</title>
-	<link rel="stylesheet" href="../css/style.css">
+	<title>3 Pears & A Lime</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<div id="page-container" class="sidebar-none site-nav-none">
 		<header class="column">
 			<section class="col">
-				<h1 class="site-title text-center">3 Pears, 1 Lime</h1>
+				<h1 class="site-title text-center">3 Pears <span class="ampersand">&amp;</span> A Lime</h1>
 			</section>
 			<section id="pears-lime" class="col no-pad no-margin">
-				<img src="../images/3 Pears 1 Lime - Header Fruit.png" alt="">
+				<img src="images/3 Pears 1 Lime - Header Fruit.png" alt="">
 			</section>
 		</header>
-		<main class="column">
+		<main class="row">
 			<article id="find" class="col">
 				<section class="container">
 					<h2>Find a <span>Recipe</span></h2>
-					<img src="../images/FancySalad.png" alt="">
+					<img src="images/FancySalad.png" alt="">
 				</section>
 			</article>
 			<article id="explore" class="col">
 				<section class="container">
-					<h2>Explore Cooking</h2>
-					<img src="../images/pancakes.png" alt="">
+					<h2>Explore <span>Cooking</span></h2>
+					<img src="images/pancakes.png" alt="">
 				</section>
 			</article>
 			<article id="submit" class="col">
 				<section class="container">
-					<h2>Submit a Recipe</h2>
-					<img src="../images/Recipe Cards.png" alt="">
+					<h2>Submit a <span>Recipe</span></h2>
+					<img src="images/Fancy Salad 2.png" alt="">
 				</section>
 			</article>
 		</main>
 		<aside class="column">
 			<article id="cocktails" class="col">
 				<section class="container">
-					<img src="../images/Lime - cocktails.png" alt="">
+					<img src="images/Lime - cocktails.png" alt="">
 					<h3>Cocktails</h3>
 				</section>
+				<section class="container">
+					<p class="margin-xl-bottom pad-xl-bottom">Get our 'Pear + Lime' Cocktails booklet <strong><em>FREE</em></strong> on orders over $30, or if you schedule more than 6 deliveries a month!</p>
+				</section>
 			</article>
-			<article id="contact" class="col">
-				<h3>Contact</h3>
+			<article id="contact" class="col pad-xl-bottom">
+				<section class="container pad-xl-bottom">
+					<h2 class="no-pad-bottom no-margin-bottom">Contact</h2>
+					
+					<div class="fieldset fieldset-xs-8 fieldset-lg-5">
+						<div class="field-group width-xs-8">
+							<label for="Name">Name</label>
+							<input type="text" name="Name" class="input input-text">
+						</div>
+						<div class="field-group width-xs-8 np-pad-bottom">
+							<label for="Email">Email</label>
+							<input type="email" name="email" class="input input-email">
+						</div>
+					</div>
+					<div class="fieldset fieldset-xs-8 fieldset-lg-7">
+						<div class="field-group width-xs-8">
+							<label for="Message">Message</label>
+							<textarea name="Message" id="message" class="input input-textarea"></textarea>
+						</div>
+					</div>
+				</section>
 			</article>
 		</aside>
 		<footer class="row copyright">
