3dced6aedf20e4bc5918ebf1f7d61598d576cf19
Margaret Ralston <pandahart@gmail.com>
2019-12-18 19:32:53 -0800

    added viewport meta to head.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>3 Pears & A Lime</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
